d5c06339f3ef8e9a3b7534e9225e12ebdd205c8b
Khaleel Jageer <jskcse4@gmail.com>
2017-05-30 13:13:47 +0530

    app gradle update

--- app/src/main/java/com/jskhaleel/myflickgallery/webservice/Webservice.java
+++ app/src/main/java/com/jskhaleel/myflickgallery/webservice/Webservice.java
@@ -2,6 +2,6 @@ package com.jskhaleel.myflickgallery.webservice;
 
 
 public class Webservice {
-    public static String FLICKR_GALLARY = "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=3a78acbf5948f373f5423648eb221725&gallery_id=72157667581365124&format=json&nojsoncallback=1";
+    public static String FLICKR_GALLARY = "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=YOUR_API_KEY&gallery_id=GALLERY_ID&format=json&nojsoncallback=1";
     public static String FLICKR_PHOTO = "https://farm%s.staticflickr.com/%s/%s_%s_n.jpg";
 }
